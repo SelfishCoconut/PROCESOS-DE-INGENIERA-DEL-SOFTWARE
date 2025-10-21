03b59c266cc8503dfc1fc50bf9f8032f0269b7d9
alvaro <alvaro>
2025-10-21 19:05:56 +0200

    inicialización de los tests más básicos con Jasmine

---
+++ client/modelo.js
@@ -0,0 +1,28 @@
+function Sistema() {
+  this.usuarios = {};
+  this.agregarUsuario = function (nick) {
+    this.usuarios[nick] = new Usuario(nick);
+  };
+
+  this.obtenerUsuarios=function(){
+    return this.usuarios;
+  }
+
+  this.usuarioActivo = function (nick) {
+    return this.usuarios.hasOwnProperty(nick);
+  }
+
+  this.eliminarUsuario = function (nick) {
+    if (this.usuarioActivo(nick)) {
+      delete this.usuarios[nick];
+    }
+  }  
+
+  this.numeroUsuarios = function () {
+    return Object.keys(this.usuarios).length;
+  }
+
+}
+function Usuario(nick) {
+  this.nick = nick;
+}
